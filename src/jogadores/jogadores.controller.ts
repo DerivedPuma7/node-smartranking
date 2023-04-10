c5bb8aa4a1e55601be194eee7a63d4abe601a39b
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-04-10 17:14:44 -0300

    feat: create jogadores controller

---
+++ src/jogadores/jogadores.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Post, Body } from '@nestjs/common';
+import { CriarJogadorDto } from './dtos/criar-jogador.dto';
+
+@Controller('api/v1/jogadores')
+export class JogadoresController {
+  @Post()
+  async criarAtualizarJogador(@Body() criarJogadorDto: CriarJogadorDto) {
+    const { email, nome, telefoneCelular } = criarJogadorDto;
+    return { email, nome, telefoneCelular };
+  }
+}
