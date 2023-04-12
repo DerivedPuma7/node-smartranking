5760b3689f9e3314276e3775c2e8b15494bfd34e
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-04-12 10:43:11 -0300

    feat: generate categorias module, controller and service

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,14 @@ import { JogadoresModule } from './jogadores/jogadores.module';
 
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
+import { CategoriasModule } from './categorias/categorias.module';
 import 'dotenv/config';
 
 @Module({
   imports: [
     MongooseModule.forRoot(process.env.DATABASE_CONECTION_URL),
     JogadoresModule,
+    CategoriasModule,
   ],
   controllers: [],
   providers: [],
