a904415917a9443496c690394203a9b720d0839f
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-04-12 11:50:06 -0300

    feat: register mongodb schema in categoria module

--- src/categorias/categorias.module.ts
+++ src/categorias/categorias.module.ts
@@ -1,9 +1,15 @@
-import { Module } from '@nestjs/common';
 import { CategoriasController } from './categorias.controller';
 import { CategoriasService } from './categorias.service';
+import { CategoriaSchema } from './interface/categoria.schema';
+
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'Categoria', schema: CategoriaSchema }]),
+  ],
   controllers: [CategoriasController],
-  providers: [CategoriasService]
+  providers: [CategoriasService],
 })
 export class CategoriasModule {}
