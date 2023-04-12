227b790fad2d87c0783301e87c87ba58a9be4f4f
Gustavo Figueiredo <gustavoribeiro665@hotmail.com>
2023-04-12 11:49:11 -0300

    feat: create categoria dtp

---
+++ src/categorias/dto/criar-categoria.dto.ts
@@ -0,0 +1,17 @@
+import { Evento } from '../interface/categoria.interface';
+
+import { IsString, IsNotEmpty, IsArray, ArrayMinSize } from 'class-validator';
+
+export class CriarCategoriaDto {
+  @IsString()
+  @IsNotEmpty()
+  readonly categoria: string;
+
+  @IsString()
+  @IsNotEmpty()
+  descricao: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  eventos: Array<Evento>;
+}
